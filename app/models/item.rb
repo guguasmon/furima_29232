1f28e84e4f5d25b6ba2a5fb0bcf76e5b55681ba6
guguasmon <hutagoazkey@gmail.com>
2020-08-23 21:07:33 +0900

    usersテーブルとitemsテーブルのアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :delivery_type
   belongs_to_active_hash :duration
   has_one_attached :image
+  belongs_to :user
 
   #空の投稿を保存できないようにする
   validates :image, :name, :text, :price, :category_id, :state_id, :derivery_type_id, :prefecture_id, :duration_id, presence: true
