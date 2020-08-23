c69ea77c8adecae0a4c6927cac2ce5fc9407ae45
guguasmon <hutagoazkey@gmail.com>
2020-08-23 21:41:15 +0900

    authenticate_user!　appとitems#new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :authenticate_user!
 
   private
 
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: :new
   def index
     @items = Item.all
     # includes(:order).order("created_at DESC")
