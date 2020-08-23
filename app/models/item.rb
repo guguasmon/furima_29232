f96b6eb4b1e90073309f269cd5a2a34ebbe49763
guguasmon <hutagoazkey@gmail.com>
2020-08-23 15:41:41 +0900

    ActiveHash stateの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,10 +2,8 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :category
+  belongs_to_active_hash :state
 
   #空の投稿を保存できないようにする
   validates :image, :name, :text, :price, :category, :state, :derivery_type, :prefecture, :duration, presence: true
-
-  #ジャンルの選択が「--」の時は保存できないようにする
-  validates :prefecture_id, numericality: { other_than: 1 } 
 end
