af5e1730ee2a6ada90290880386dd84e23bd57ec
guguasmon <hutagoazkey@gmail.com>
2020-08-23 15:49:43 +0900

    ActiveHash durationの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :category
   belongs_to_active_hash :state
   belongs_to_active_hash :derivery_type
+  belongs_to_active_hash :duration
 
   #空の投稿を保存できないようにする
   validates :image, :name, :text, :price, :category_id, :state_id, :derivery_type_id, :prefecture_id, :duration_id, presence: true
