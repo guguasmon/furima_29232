133967f866f58e47033c1342df2415821cfb36f7
guguasmon <hutagoazkey@gmail.com>
2020-08-23 15:25:35 +0900

    ActiveHash prefectureの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :presence
+
+  #空の投稿を保存できないようにする
+  validates :image, :name, :text, :price, :category, :state, :derivery_type, :prefecture, :duration, presence: true
+
+  #ジャンルの選択が「--」の時は保存できないようにする
+  validates :prefecture_id, numericality: { other_than: 1 } 
 end
