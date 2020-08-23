8b504d113e2f5e3b1bab167183e0b780ebc9615d
guguasmon <hutagoazkey@gmail.com>
2020-08-23 15:46:18 +0900

    ActiveHash delivery_typeの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,8 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :category
   belongs_to_active_hash :state
+  belongs_to_active_hash :derivery_type
 
   #空の投稿を保存できないようにする
-  validates :image, :name, :text, :price, :category, :state, :derivery_type, :prefecture, :duration, presence: true
+  validates :image, :name, :text, :price, :category_id, :state_id, :derivery_type_id, :prefecture_id, :duration_id, presence: true
 end
