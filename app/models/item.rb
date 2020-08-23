7357ee749a529f14c9eec6a07211a5b175dd0fe8
guguasmon <hutagoazkey@gmail.com>
2020-08-23 15:34:41 +0900

    ActiveHash categoryの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :presence
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :category
 
   #空の投稿を保存できないようにする
   validates :image, :name, :text, :price, :category, :state, :derivery_type, :prefecture, :duration, presence: true
