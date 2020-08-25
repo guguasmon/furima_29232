e5623671089db75bea4ae6896d0123936a4fbea7
guguasmon <hutagoazkey@gmail.com>
2020-08-25 09:59:36 +0900

    userバリデーションメッセージ修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,12 @@ class User < ApplicationRecord
   has_many :items
   with_options presence: true do
     validates :name
-    validates :email, uniqueness: { case_sensitive: true }, format: { with: /\A.+@.+\z/, message: "@マークを入れてください"}
-    validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "半角英数字混合です"}
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "全角（漢字・ひらがな・カタカナ）で入力"}
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "全角（漢字・ひらがな・カタカナ）で入力"}
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角（カタカナ）で入力"}
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角（カタカナ）で入力"}
+    validates :email, uniqueness: { case_sensitive: true }, format: { with: /\A.+@.+\z/}
+    validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "Password Include both letters and numbers"}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "full-width characters."}
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "full-width characters."}
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "full-width katakana characters."}
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "full-width katakana characters."}
     validates :birth_date
   end
 end
