51ec1d136f0a9e4859b383561d1de00de8f0abca
guguasmon <hutagoazkey@gmail.com>
2020-08-25 16:46:16 +0900

    items#createアクション実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,9 +9,19 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
 
   def item_params
-    # params.require(:item).permit(,:image)
+    params.require(:item).permit(:image, :name, :text, :category_id, :state_id, :delivery_type_id, :prefecture_id, :duration_id, :price).merge(user_id: current_user.id)
   end
 end
