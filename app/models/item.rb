b235205839ee6ea2cc166bb07e6f1cc537f0301c
guguasmon <hutagoazkey@gmail.com>
2020-08-25 14:22:46 +0900

    itemモデルのバリデーションをwith_optionsで指定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,11 +3,32 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :category
   belongs_to_active_hash :state
-  belongs_to_active_hash :delivery_type
+  belongs_to_active_hash :deliveryType
   belongs_to_active_hash :duration
   has_one_attached :image
   belongs_to :user
 
   # 空の投稿を保存できないようにする
-  validates :image, :name, :text, :price, :category_id, :state_id, :derivery_type_id, :prefecture_id, :duration_id, presence: true
+  # validates :image, :name, :text, :price, :category_id, :state_id, :delivery_type_id, :prefecture_id, :duration_id, presence: true
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :text
+    validates :price
+    validates :category_id
+    validates :state_id
+    validates :delivery_type_id
+    validates :prefecture_id
+    validates :duration_id
+    #リストの選択が「--」の時は保存できないようにする
+    with_options numericality: { other_than: 0, message: "can't be blank"  }  do
+    validates :category_id
+    validates :state_id
+    validates :delivery_type_id
+    validates :prefecture_id
+    validates :duration_id
+    end 
+    # 「値段」の金額範囲に関するバリデーション
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
+  end
 end
