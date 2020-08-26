6b50bd9e136cc015f3623ca5da373aaca1971ed0
guguasmon <hutagoazkey@gmail.com>
2020-08-26 14:17:14 +0900

    バリデーションのコメントアウト削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,8 +8,6 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :user
 
-  # 空の投稿を保存できないようにする
-  # validates :image, :name, :text, :price, :category_id, :state_id, :delivery_type_id, :prefecture_id, :duration_id, presence: true
   with_options presence: true do
     validates :image
     validates :name, length: { maximum: 40 }
@@ -20,7 +18,6 @@ class Item < ApplicationRecord
     validates :delivery_type_id
     validates :prefecture_id
     validates :duration_id
-    # リストの選択が「--」の時は保存できないようにする
     with_options numericality: { other_than: 0, message: "can't be blank" } do
       validates :category_id
       validates :state_id
@@ -28,7 +25,6 @@ class Item < ApplicationRecord
       validates :prefecture_id
       validates :duration_id
     end
-    # 「値段」の金額範囲に関するバリデーション
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
   end
 end
