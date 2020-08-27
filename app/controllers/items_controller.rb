37b145dd9ff1f57a24239b8700ab651a1c05a196
guguasmon <hutagoazkey@gmail.com>
2020-08-27 11:21:07 +0900

    削除機能実装後、Rubocopを実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
   before_action :set_item, only: [:edit, :show]
   def index
-    @items = Item.all.order("created_at DESC")
+    @items = Item.all.order('created_at DESC')
   end
 
   def new
