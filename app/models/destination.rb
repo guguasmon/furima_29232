be967893eb4fb83a70d721db7f2fac5e6ad94dd7
guguasmon <hutagoazkey@gmail.com>
2020-08-27 19:27:43 +0900

    destinationのバリデーション

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,10 @@
 class Destination < ApplicationRecord
   belongs_to :order
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :prefecture_id
+    validates :city
+    validates :house_number
+    validates :phone_number, format: { with: /\A\d{11}\z/, message: 'is invalid. Include not hyphen(-)' }
+  end
 end
