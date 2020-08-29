1ac9e7b8292812fae7510aa6ccb3b92ea5e9dbb6
guguasmon <hutagoazkey@gmail.com>
2020-08-29 11:25:28 +0900

    orderモデルのテストコード

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,6 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :item
   has_one    :destination
+
+  validates :token, presence: true
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @order = FactoryBot.build(:order)
+  end
+
+  describe 'オーダー登録' do
+    context '登録がうまくいくとき' do
+      it 'tokenが存在すれば登録できる' do
+        expect(@order).to be_valid
+      end
+    end
+    context '登録がうまくいかない時' do
+      it 'tokenが空だと登録できない' do
+        @order.token = nil
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Token can't be blank")
+      end
+    end
+  end
 end
