fb174aabdc784ec218c63feeb0867285980ec833
guguasmon <hutagoazkey@gmail.com>
2020-08-29 09:36:41 +0900

    ActiveHash　UserShoppingクラス

--- app/forms/user_shopping.rb
+++ app/forms/user_shopping.rb
@@ -1,7 +1,7 @@
 class UserShopping
 
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :item_id, :user_id, :order_id
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :item_id, :user_id, :order_id, :token
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'is invalid. Include hyphen(-)' }
@@ -9,13 +9,19 @@ class UserShopping
     validates :city
     validates :house_number
     validates :phone_number, format: { with: /\A\d{11}\z/, message: 'is invalid. Include not hyphen(-)' }
+    validates :token
   end
 
   def save
-    order = Order.new(item_id: item_id, user_id: user_id)
-    destination = Destination.new(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id )
-    if destination.save
+    order = Order.new(token: token, item_id: item_id, user_id: user_id)
+    if order.valid?
       order.save
+      destination = Destination.new(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id )
+      if destination.valid?
+        destination.save
+      else
+        order.destroy
+      end
     end
   end
 end
