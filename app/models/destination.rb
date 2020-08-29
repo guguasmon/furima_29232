7760e385b11c2f97705a12d2a6deb0ce36752b66
guguasmon <hutagoazkey@gmail.com>
2020-08-29 12:17:54 +0900

    Destinationモデルのテストコード

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -3,7 +3,7 @@ class Destination < ApplicationRecord
   
   with_options presence: true do
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'is invalid. Include hyphen(-)' }
-    validates :prefecture_id
+    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" } 
     validates :city
     validates :house_number
     validates :phone_number, format: { with: /\A\d{11}\z/, message: 'is invalid. Include not hyphen(-)' }
